07e611c405ee04cba1d837deb3d5e3b06c8b269e
HwanGonJang <myggona@gmail.com>
2024-12-19 14:23:08 +0900

    feat: 경신스 전체 조회

---
+++ tle/tle-application/src/main/kotlin/com/example/tleapplication/application/scrap/ScrapListResponse.kt
@@ -0,0 +1,5 @@
+package com.example.tleapplication.application.scrap
+
+data class ScrapListResponse(
+    val scrapList: List<ScrapResponse>
+)
--- tle/tle-domain/src/main/kotlin/com/example/tleapplication/domain/scrap/ScrapRepository.kt
+++ tle/tle-domain/src/main/kotlin/com/example/tleapplication/domain/scrap/ScrapRepository.kt
@@ -2,6 +2,7 @@ package com.example.tleapplication.domain.scrap
 
 import com.example.tleapplication.domain.news.News
 import com.example.tleapplication.domain.user.User
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -9,6 +10,7 @@ interface ScrapRepository {
     fun save(scrap: Scrap, user: User, news: News): Scrap
     fun findScrapByUserAndNews(userId: Long, newsId: Long): Scrap?
     fun findScrapById(id: Long): Scrap
+    fun findAllScraps(userId: Long, pageable: Pageable): List<Scrap>
     fun update(scrap: Scrap): Scrap
     fun delete(id: Long)
 }
